05c70245ead715fe8098e60f4958dcdfd1a1c2d6
hos1r1s <hos1r1s.2001@gmail.com>
2023-06-20 11:12:33 -0300

    v1.5

--- src/app/sharepage/box-login/box-login.component.ts
+++ src/app/sharepage/box-login/box-login.component.ts
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-=======
 import { Component, OnInit } from '@angular/core';
 import { ServicoLogin } from 'src/app/services/servico-login';
 import  {  FormBuilder,  FormGroup  }  from  '@angular/forms';
->>>>>>> 5d05e58 (v1.2-notebook)
 
 @Component({
   selector: 'app-box-login',
   templateUrl: './box-login.component.html',
   styleUrls: ['./box-login.component.css']
 })
-<<<<<<< HEAD
-export class BoxLoginComponent {
-=======
 export class BoxLoginComponent implements OnInit {
   
   formLogin!: FormGroup;
@@ -35,6 +28,5 @@ export class BoxLoginComponent implements OnInit {
   onSubmit(){
     console.log(this.formLogin.value);
   }
->>>>>>> 5d05e58 (v1.2-notebook)
 
 }
