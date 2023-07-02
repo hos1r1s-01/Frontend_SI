9a308ac92158a4b87b60d79a31369a91ffee5f6c
Vitor <vitor.duda@zenvia.com>
2023-07-02 17:58:24 -0300

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,21 +7,20 @@ import { TelaInicialComponent } from './pages/tela-inicial/tela-inicial.componen
 import { ContatoComponent } from './pages/contato/contato.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
-import { CadastroFeedbackDisciplinaComponent } from './pages/cadastro-feedback-disciplina/cadastro-feedback-disciplina.component';
-import { CadastroFeedbackProfessorComponent } from './pages/cadastro-feedback-professor/cadastro-feedback-professor.component';
+import { CadastroFeedbackComponent } from './pages/cadastro-feedback/cadastro-feedback.component';
 import { FeedbackDisciplinaComponent } from './pages/feedback-disciplina/feedback-disciplina.component';
 import { FeedbackProfessorComponent } from './pages/feedback-professor/feedback-professor.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent},
+  {path: '', redirectTo: '/auth', pathMatch: 'full'},
+  {path:'auth', component:LoginComponent},
   {path:'cadastro-usuario', component:CadastroUsuarioComponent},
   {path:'primeiro-login', component:PrimeiroLoginComponent},
   {path:'tela-inicial', component:TelaInicialComponent},
   {path:'contato', component:ContatoComponent},
   {path:'faq', component:FaqComponent},
   {path:'sobre-nos', component:SobreNosComponent},
-  {path:'feedback/disciplina', component:CadastroFeedbackDisciplinaComponent},
-  {path:'feedback/professor', component:CadastroFeedbackProfessorComponent},
+  {path:'feedback/disciplina', component:CadastroFeedbackComponent},
   {path: 'consultar/disciplina', component:FeedbackDisciplinaComponent},
   {path:'consultar/professor', component:FeedbackProfessorComponent}
   //{path:'pretencao/', component:}
