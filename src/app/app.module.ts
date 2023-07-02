083d3fd3403213165fd7aeba24064d621b9e9d62
Vitor <vitor.duda@zenvia.com>
2023-07-02 17:59:33 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,16 @@ import { TelaInicialComponent } from './pages/tela-inicial/tela-inicial.componen
 import { ContatoComponent } from './pages/contato/contato.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
-import { BoxLoginComponent } from './sharepage/box-login/box-login.component';
 import { BoxCadastroComponent } from './sharepage/box-cadastro/box-cadastro.component';
 import { BoxPrimeiroLoginComponent } from './sharepage/box-primeiro-login/box-primeiro-login.component';
-import { BoxCadastroDisciplinaComponent } from './sharepage/box-cadastro-disciplina/box-cadastro-disciplina.component';
-import { BoxCadastroProfessorComponent } from './sharepage/box-cadastro-professor/box-cadastro-professor.component';
+import { CardComponent } from './sharepage/card/card.component';
 import { BoxSugestaoComponent } from './sharepage/box-sugestao/box-sugestao.component';
 import { TesteForComponent } from './sharepage/teste-for/teste-for.component';
 import { HeaderFeedProfessorComponent } from './sharepage/header-feed-professor/header-feed-professor.component';
 import { FeedbackProfessorComponent } from './pages/feedback-professor/feedback-professor.component';
 import { FeedbackDisciplinaComponent } from './pages/feedback-disciplina/feedback-disciplina.component';
-import { CadastroFeedbackProfessorComponent } from './pages/cadastro-feedback-professor/cadastro-feedback-professor.component';
-import { CadastroFeedbackDisciplinaComponent } from './pages/cadastro-feedback-disciplina/cadastro-feedback-disciplina.component';
+import { CadastroFeedbackComponent } from './pages/cadastro-feedback/cadastro-feedback.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -38,23 +36,21 @@ import { CadastroFeedbackDisciplinaComponent } from './pages/cadastro-feedback-d
     ContatoComponent,
     FaqComponent,
     SobreNosComponent,
-    BoxLoginComponent,
     BoxCadastroComponent,
     BoxPrimeiroLoginComponent,
-    BoxCadastroDisciplinaComponent,
-    BoxCadastroProfessorComponent,
+    CardComponent,
     BoxSugestaoComponent,
     TesteForComponent,
     HeaderFeedProfessorComponent,
     FeedbackProfessorComponent,
     FeedbackDisciplinaComponent,
-    CadastroFeedbackProfessorComponent,
-    CadastroFeedbackDisciplinaComponent
+    CadastroFeedbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
